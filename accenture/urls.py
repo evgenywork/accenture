87e06512f89ad43deb5790b457650428501641ba
eugen <5584040@gmail.com>
2020-04-26 14:56:07 +0300

    add scripts

--- accenture/urls.py
+++ accenture/urls.py
@@ -5,5 +5,6 @@ from .views import *
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('all/', get_main_json, name='get_main_json_url'),
+    path('rg/<str:resource_group_id>/<str:rg_date>', get_rg_by_id_date, name='get_rg_by_id_date_url'),
     path('index/', index, name='index_url'),
 ]
